6427c6dc2758acc67752524103e9986febee7e0f
Minh-Dat Le Chon <93373784+DatMinhLeChon@users.noreply.github.com>
2022-09-08 20:24:26 +0700

    Update h_Function_Caculate.py

--- h_Function_Caculate.py
+++ h_Function_Caculate.py
@@ -1,4 +1,4 @@
-import h_FCFS as fcfs
+import h_FCFS as fcfs # lien ket toi file thu vien h_FCFS.py, moi nguoi co the copy phan duoi dong nay bo qua file kia cho de
 
 temp = input("so luong cong viec")
 
