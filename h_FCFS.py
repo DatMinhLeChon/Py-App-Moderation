b89ddd8209fc78cde68c01ebd7d17aee5b4ba43b
Minh-Dat Le Chon <93373784+DatMinhLeChon@users.noreply.github.com>
2022-09-09 00:25:44 +0700

    Update h_FCFS.py

--- h_FCFS.py
+++ h_FCFS.py
@@ -14,32 +14,32 @@ def cal_time(val_a, val_b):
 class work:
     # Khoi tao gia tri
     def _init_(self, val_a, val_b, val_c):
-        self.name_work = val_a;
-        self.time_work = val_b;
-        self.time_end = val_c;
+        self.name_work = val_a; # Ten cong viec
+        self.time_work = val_b; # Thoi gian thuc hien cong viec
+        self.time_end = val_c; # Thoi gian toi han
 
     def get_name_work(self):
-        return self.name_work
+        return self.name_work # Tra ve gia tri
     
     def get_time_work(self):
-        return self.time_work
+        return self.time_work # Tra ve gia tri
 
     def get_time_end(self):
-        return self.time_end
+        return self.time_end # Tra ve gia tri
 
 # Dinh nghia mot danh sach cac cong viec - define a list of work
 class FCFS:
     # Khoi tao gia tri
     def _init_List(self, val_a):
         self.volume = val_a;
-        self.list_work = [];
+        self.list_work = []; # Tao danh sach rong
         for i in range(0, int(self.volume)):
-            self.list_work.append(work());
+            self.list_work.append(work()); # Them mot phan tu kieu work vao danh sach
             print("Cong viec thu");
             temp1 = input("nhap ten cong viec" );
             temp2 = input("nhap thoi gian cong viec" );
             temp3 = input("nhap thoi gian toi han cua cong viec" );
-            self.list_work[i]._init_(temp1, temp2, temp3);
+            self.list_work[i]._init_(temp1, temp2, temp3); # Khoi tao gia tri cho phan tu vua them vao danh sach
     
     # Tinh cac chi so va thu tu cong viec theo FCFS
     def calculate(self):
@@ -47,8 +47,11 @@ class FCFS:
         self.total_late_time =0;
         self.process = [];
         for i in range(0, int(self.volume)):
-            self.total_time = 2*int(self.total_time) + int(self.list_work[i].time_work);
+            # Tinh Tong thoi gian cong viec tai thoi diem cong viec thu i duoc thuc hien
+            self.total_time = 2*int(self.total_time) + int(self.list_work[i].time_work); 
+            # Tinh tong thoi gian du thua cho den thoi diem cong viec i
             self.total_late_time = int(self.total_late_time) + int(cal_time(self.list_work[i].time_end, self.total_time));
+            # Them ten cong viec i vao thu tu thuc hien
             self.process.append(self.list_work[i].name_work);
     
     # Xuat ra command
