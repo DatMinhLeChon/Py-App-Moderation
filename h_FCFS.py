b31fa35122f37f9d54f4ef41f69b00448dbeac2e
Minh-Dat Le Chon <93373784+DatMinhLeChon@users.noreply.github.com>
2022-09-09 00:21:12 +0700

    Update h_FCFS.py

--- h_FCFS.py
+++ h_FCFS.py
@@ -1,14 +1,18 @@
+# File thu vien h_FCFS
+# Khai bao thu vien chuan
 import string
-# variable and class
 
+# Function, variable and class
+# Ham tra ve gia tri thoi gian tre han cua cong viec
 def cal_time(val_a, val_b):
     if int(val_b) - int(val_b) > 0:
         return 0
     else:
         return int(val_b) - int(val_a)
 
+# Dinh nghia mot cong viec - define a work
 class work:
-    ## khoi tao
+    # Khoi tao gia tri
     def _init_(self, val_a, val_b, val_c):
         self.name_work = val_a;
         self.time_work = val_b;
@@ -23,8 +27,9 @@ class work:
     def get_time_end(self):
         return self.time_end
 
+# Dinh nghia mot danh sach cac cong viec - define a list of work
 class FCFS:
-    ## khoi tao
+    # Khoi tao gia tri
     def _init_List(self, val_a):
         self.volume = val_a;
         self.list_work = [];
@@ -35,7 +40,8 @@ class FCFS:
             temp2 = input("nhap thoi gian cong viec" );
             temp3 = input("nhap thoi gian toi han cua cong viec" );
             self.list_work[i]._init_(temp1, temp2, temp3);
-            
+    
+    # Tinh cac chi so va thu tu cong viec theo FCFS
     def calculate(self):
         self.total_time =0;
         self.total_late_time =0;
@@ -45,19 +51,10 @@ class FCFS:
             self.total_late_time = int(self.total_late_time) + int(cal_time(self.list_work[i].time_end, self.total_time));
             self.process.append(self.list_work[i].name_work);
     
+    # Xuat ra command
     def show(self):
         print(self.total_time);
         print(self.total_late_time);
         print(self.process);
         
-
-
-
-
-
-
-
-
-
-    
      
